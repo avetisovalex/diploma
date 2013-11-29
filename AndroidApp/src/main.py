c8d6c1b038e675ad2f05b1401bde680e92efb27d
NataBoginia <nata_boginia@mail.ru>
2013-11-30 02:28:55 +0400

    привязана текстура

--- AndroidApp/src/main.py
+++ AndroidApp/src/main.py
@@ -7,7 +7,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.label import Label
+from kivy.uix.image import Image
 from kivy.uix.widget import Widget
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.graphics import Canvas
@@ -27,7 +29,16 @@ Config.set('graphics', 'height', '500')
 Builder.load_file('dip.kv')
 
 # Declare screens
-class LoginScreen(Screen):
+class MyScreen(Screen):
+	def show_popup(self,but_text,label_text,title_text):
+		content_popup = Button(text=but_text,size_hint=(.5,1),pos_hint={'center_x':.5,'y':.0})
+		content = BoxLayout(orientation='vertical',padding=(0,self.height*0.01))
+		content.add_widget(Label(text=label_text,halign='center',size_hint=(1,2)))
+		content.add_widget(content_popup)
+		popup = Popup(title=title_text,content=content,size_hint=(.9,.3))
+		content_popup.bind(on_press=popup.dismiss)
+		popup.open()	
+class LoginScreen(MyScreen):
 	global i
 	i=0
 	def reg(self):
@@ -36,76 +47,68 @@ class LoginScreen(Screen):
 		global i
 		i+= 1
 		if i%2:
-			content_cancel = Button(text='Ok',size_hint=(.5,1),pos_hint={'center_x':.5,'y':.0})
-			content = BoxLayout(orientation='vertical',padding=(0,self.height*0.01))
-			content.add_widget(Label(text='Неверное имя пользователя\nили пароль',halign='center',size_hint=(1,2)))
-			content.add_widget(content_cancel)
-			popup = Popup(title='Ошибка авторизации', content=content,size_hint=(.9,.3))
-			content_cancel.bind(on_press=popup.dismiss)
-			popup.open()
+			self.show_popup('Ok','Неверное имя пользователя\nили пароль','Ошибка авторизации')
 		else: 
 			self.manager.current = 'main'
 
-class CustomPopup(Popup):
-    pass
-class AuthorizationScreen(Screen):
+class AuthorizationScreen(MyScreen):
 	global i
 	i=0
 	def register(self):
 		global i
 		i+= 1
 		if i%2:
-			content_cancel = Button(text='Ok',size_hint=(.5,1),pos_hint={'center_x':.5,'y':.0})
-			content = BoxLayout(orientation='vertical',padding=(0,self.height*0.01))
-			content.add_widget(Label(text='Неверно введены данные',halign='center',size_hint=(1,2)))
-			content.add_widget(content_cancel)
-			popup = Popup(title='Ошибка заполнения формы', content=content,size_hint=(.9,.3))
-			content_cancel.bind(on_press=popup.dismiss)
-			popup.open()
+			self.show_popup('Ok','Неверно введены данные','Ошибка заполнения формы')
 		else: 
-			content_cancel = Button(text='Ok',size_hint=(.5,1),pos_hint={'center_x':.5,'y':.0})
-			content = BoxLayout(orientation='vertical',padding=(0,self.height*0.01))
-			content.add_widget(Label(text='Спасибо за регистрацию!',halign='center',size_hint=(1,2)))
-			content.add_widget(content_cancel)
-			popup = Popup(title='Регистрация успешна', content=content,size_hint=(.9,.3))
-			content_cancel.bind(on_press=popup.dismiss)
-			popup.open()
+			self.show_popup('Ok','Спасибо за регистрацию!','Регистрация успешна')
 			self.manager.current = 'login'
 	def code_sms(self):
-		content_cancel = Button(text='Ok',size_hint=(.5,1),pos_hint={'center_x':.5,'y':.0})
-		content = BoxLayout(orientation='vertical',padding=(0,self.height*0.01))
-		content.add_widget(Label(text='Вам отправлено смс-сообщение\nс кодом подтверждения',halign='center',size_hint=(1,2)))
-		content.add_widget(content_cancel)
-		popup = Popup(title='Подтверждение регистрации', content=content,size_hint=(.9,.3))
-		content_cancel.bind(on_press=popup.dismiss)
-		popup.open()
-	
+		self.show_popup('Ok','Вам отправлено смс-сообщение\nс кодом подтверждения','Подтверждение регистрации')
 
-    
-class MainScreen(Screen):
+class MainScreen(MyScreen):
     pass
-class TasksScreen(Screen):
+class TasksScreen(MyScreen):
     pass
-class TaskEditScreen(Screen):
+class TaskEditScreen(MyScreen):
 	
 	def on_pre_enter(self):
-		self.ids.grid.add_widget(Label(text='hdjlg'))
+		portfolio=[]
+		self.ids.grid.clear_widgets()
+		for line in open('1.txt'):
+			fields=line.split(',')
+			name=fields[0]
+			stock=(name)
+			portfolio.append(stock)
+		self.ids.grid.add_widget(Label())
+		self.ids.grid.add_widget(Image(source='1.png',allow_stretch=True))
+		self.ids.grid.add_widget(Label())
+		self.ids.grid.add_widget(Image(source='1.png',allow_stretch=True))
+		for name in portfolio:
+			self.ids.grid.add_widget(Label(text=str(name),halign='left',size_hint=(3,1)))
+			self.ids.grid.add_widget(CheckBox(id=name+'space'))
+			self.ids.grid.add_widget(Label())
+			self.ids.grid.add_widget(CheckBox(id=name+'sms'))
+	def create_task(self):
+		self.show_popup('Ok','Задание опубликовано!','Добавление задания')
+		self.manager.current = 'tasks'
     
-class ProductsScreen(Screen):
+class ProductsScreen(MyScreen):
     pass
-class ProductsInfScreen(Screen):
+class ProductsInfScreen(MyScreen):
     pass
-class SettingsScreen(Screen):
+class SettingsScreen(MyScreen):
     pass
-class StatisticsScreen(Screen):
+class StatisticsScreen(MyScreen):
     pass
-class MenuScreen(Screen):
+class MenuScreen(MyScreen):
+
     def on_exit(self):
 		exit()
-class AboutScreen(Screen):
+class AboutScreen(MyScreen):
     pass
-'''class MyScreenManager(ScrollView, ScreenManager):
-	def swipe(self,touch):
+#class MyScreenManager(Carousel, ScreenManager):
+#	pass
+'''	def swipe(self,touch):
 		self.touch_start = touch.pos
 		if self.do_scroll_x and self.effect_x:
 			self.effect_x.start(touch.x)
@@ -119,10 +122,12 @@ class AboutScreen(Screen):
 
 return	
 '''
+    
 class DipApp(App):
+	
 	def build(self):
 		sm = ScreenManager()
-		sm.transition = SlideTransition(direction="down")
+		sm.transition = SlideTransition(direction='down')
 		sm.add_widget(LoginScreen(name='login'))
 		sm.add_widget(AuthorizationScreen(name='authorization'))
 		sm.add_widget(MainScreen(name='main'))
